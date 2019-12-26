06f51794e98c8cf6553bff47950c7e1beeff28fc
kevald1963 <kevin_alderson@yahoo.co.uk>
2019-12-26 17:28:19 +0000

    _Project Documentation folder created. Database Collections spreadsheet added to it. Updated run.py to cater for recent name change to Recipes list html template.

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/list_recipes')
 def list_recipes():
-    return render_template("list_recipes.html", recipes=list(mongo.db.recipes.find()))
+    return render_template("recipes_list.html", recipes=list(mongo.db.recipes.find()))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
