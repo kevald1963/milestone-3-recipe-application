ef4565d64a7d7b56c6e7202ca1ac584e33a8cd7d
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-01-23 19:40:25 +0000

    Created edit_recipe_category.html.

--- run.py
+++ run.py
@@ -91,19 +91,26 @@ def convert_from_gas_mark(temperature_value):
     user_temperature_type = 3
     return create_temperature_object(celsius, celsius_fan, fahrenheit, gas_mark, user_temperature_type)
 
+# Show the 'Home' page. 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template("index.html", recipe=mongo.db.recipes.find_one({"popular_recipe": True, "archived": False}))
 
+# Show the 'Recipes' page. 
+# Invoked primarily from the Nav bar.
 @app.route('/recipes')
 def recipes():
     return render_template("recipes.html", recipes=list(mongo.db.recipes.find({"archived": False})))
 
+# Show the 'Add recipe' page. 
+# Invoked from 'Recipes' page.
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("add_recipe.html", recipe_categories=list(mongo.db.recipe_categories.find()), usernames=list(mongo.db.users.find()))
 
+# Insert the recipe and return to the 'Recipes' page.
+# Invoked from 'Add recipe' page.
 @app.route('/insert_recipe', methods=["POST"])
 def insert_recipe():
     recipes = mongo.db.recipes
@@ -137,6 +144,8 @@ def insert_recipe():
     return redirect(gitpod_url + 'recipes')
     #return redirect(url_for('recipes'))
 
+# Show the 'View recipe' page.
+# Invoked from 'Recipes' page.
 @app.route('/view_recipe/<_id>')
 def view_recipe(_id):
     _recipe = mongo.db.recipes.find_one({"_id": ObjectId(_id)})
@@ -163,6 +172,8 @@ def view_recipe(_id):
     recipe_category_list = [recipe_category for recipe_category in _recipe_categories]
     return render_template("view_recipe.html", recipe = _recipe, user_temperature_type = _user_temperature_type, temperature_value = _temperature_value, recipe_categories = recipe_category_list, usernames=list(mongo.db.users.find()))
 
+# Show the 'Edit recipe' page.
+# Invoked from 'Recipes' page.
 @app.route('/edit_recipe/<_id>')
 def edit_recipe(_id):
     _recipe = mongo.db.recipes.find_one({"_id": ObjectId(_id)})
@@ -189,6 +200,8 @@ def edit_recipe(_id):
     recipe_category_list = [recipe_category for recipe_category in _recipe_categories]
     return render_template("edit_recipe.html", recipe = _recipe, user_temperature_type = _user_temperature_type, temperature_value = _temperature_value, recipe_categories = recipe_category_list, usernames=list(mongo.db.users.find()))
 
+# Update the recipe and return to the 'Recipes' page.
+# Invoked from 'Edit recipe' page.
 @app.route('/update_recipe/<_id>', methods=["POST"])
 def update_recipe(_id):
     recipes = mongo.db.recipes
@@ -221,6 +234,8 @@ def update_recipe(_id):
     #return redirect(gitpod_url + 'recipes')
     return redirect(url_for('recipes'))
 
+# Set the recipe to 'archived', so that it's hidden, and return to the 'Recipes' page.
+# Invoked from 'Recipes' page.
 @app.route('/archive_recipe/<_id>')
 def archive_recipe(_id):
     recipes = mongo.db.recipes
@@ -233,8 +248,9 @@ def archive_recipe(_id):
     
     # Refresh recipes page now that recipe has been archived and should no longer be displayed.
     return redirect(url_for('recipes'))
-    #return render_template("recipes.html", recipes=list(mongo.db.recipes.find({"archived": False})))
 
+# Delete the recipe and return to the 'Recipes' page.
+# Invoked from 'Recipes' page.
 @app.route('/delete_recipe/<_id>')
 def delete_recipe(_id):
 
@@ -245,19 +261,51 @@ def delete_recipe(_id):
     return redirect(gitpod_url + 'recipes')
     #return redirect(url_for('recipes'))
 
-# Show the 'Add recipe category' page.
+# Show the 'Recipe category' page. 
+# Invoked primarily from the Nav bar.
+@app.route('/recipe_categories')
+def recipe_categories():
+    return render_template("recipe_categories.html", recipe_categories=list(mongo.db.recipe_categories.find()))
+
+# Show the 'Add recipe category' page. 
+# Invoked from 'Category' page.
 @app.route('/add_recipe_category')
 def add_recipe_category():
     return render_template("add_recipe_category.html")
 
-# Insert the recipe category and return to the Category List page. -->
+# Insert the recipe category and return to the 'Recipe categories' page.
+# Invoked from 'Add recipe category' page.
 @app.route('/insert_recipe_category', methods=["POST"])
 def insert_recipe_category():
-    categories = mongo.db.recipe_categories
-    category = {"category_name": request.form.get('category_name')}
-    categories.insert_one(category)
-    return redirect(gitpod_url + 'home')
-    #return redirect(url_for('home'))
+    recipe_categories = mongo.db.recipe_categories
+    recipe_category = {"category_name": request.form.get('category_name')}
+    recipe_categories.insert_one(recipe_category)
+    return redirect(gitpod_url + 'recipe_categories')
+    #return redirect(url_for('recipe_categories'))
+
+# Show the 'Edit recipe category' page.
+# Invoked from 'Recipe category' page.
+@app.route('/edit_recipe_category/<recipe_category_id>')
+def edit_category(recipe_category_id):
+    return render_template("edit_recipe_category.html", recipe_category = mongo.db.recipe_categories.find_one({"_id": ObjectId(recipe_category_id)}))
+
+# Update the recipe category and return to the 'Recipe categories' page.
+# Invoked from 'Edit recipe category' page.
+@app.route('/update_recipe_category/<recipe_category_id>', methods=['POST'])
+def update_recipe_category(recipe_category_id):
+    mongo.db.recipe_categories.update(
+        {'_id': ObjectId(recipe_category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(gitpod_url + 'recipe_categories')
+    #return redirect(url_for('recipe_categories'))
+
+# Delete the recipe category and return to the 'Recipe categories' page.
+# Invoked from 'Recipe category' page.
+@app.route('/delete_recipe_category/<recipe_category_id>')
+def delete_recipe_category(recipe_category_id):
+    mongo.db.recipe_categories.delete_one({'_id': ObjectId(recipe_category_id)})
+    return redirect(gitpod_url + 'recipe_categories')
+    #return redirect(url_for('recipe_categories'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
