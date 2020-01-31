d801c8b8a0e86604f4b7ac413c003de79af7a804
kevald1963 <kevin_alderson@yahoo.co.uk>
2020-01-31 18:36:00 +0000

    Improved / moved commenting, shortened lines (to comply with PEP8 standards) in run.py.

--- run.py
+++ run.py
@@ -6,7 +6,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
 from utils.general import test_checker, string_to_boolean, roundup_nearest_ten, roundup_nearest_one
-from utils.temperature_conversions import compute_temperature_settings, create_temperature_object, convert_from_celsius, convert_from_celsius_fan, convert_from_fahrenheit, convert_from_gas_mark
+from utils.temperature_conversions import compute_temperature_settings, create_temperature_object, 
+     convert_from_celsius, convert_from_celsius_fan, convert_from_fahrenheit, convert_from_gas_mark
 
 app = Flask(__name__)
 
@@ -15,10 +16,10 @@ app.config["MONGO_DBNAME"] = "baking_hot"
 
 mongo = PyMongo(app)
 
-# Show the 'Home' page. 
 @app.route('/')
 @app.route('/home')
 def home():
+    """ Show the 'Home' page. """
 
     # Can't get this to display a randomly selected record, yet the print shows it's 
     # finding data. Why?
@@ -31,23 +32,24 @@ def home():
 
     return render_template("index.html", recipe=mongo.db.recipes.find_one({"popular_recipe": True, "archived": False}))
 
-# Show the 'Recipes' page. 
-# Invoked primarily from the Nav bar.
 @app.route('/recipes')
 def recipes():
+    """ Show the 'Recipes' page.
+    Invoked primarily from the Nav bar. """
     return render_template("recipes.html", recipes=list(mongo.db.recipes.find({"archived": False}).sort("category", 1)))
 
-# Show the 'Add recipe' page. 
-# Invoked from 'Recipes' page.
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("add_recipe.html", recipe_categories=list(mongo.db.recipe_categories.find()), usernames=list(mongo.db.users.find()))
+    """ Show the 'Add recipe' page. 
+    Invoked from 'Recipes' page. """
+    return render_template("add_recipe.html", recipe_categories=list(mongo.db.recipe_categories.find()), 
+                            usernames=list(mongo.db.users.find()))
 
-# Insert the recipe and return to the 'Recipes' page.
-# Invoked from 'Add recipe' page.
 @app.route('/insert_recipe', methods=["POST"])
 def insert_recipe():
     recipes = mongo.db.recipes
+    """ Insert the recipe and return to the 'Recipes' page.
+    Invoked from 'Add recipe' page. """
     
     # Multi-line input elements need converted to a list before saving to MongoDB.
     ingredients = request.form.getlist("ingredients[]")
@@ -79,17 +81,19 @@ def insert_recipe():
     )    
     return redirect(url_for('recipes'))
 
-# Show either the 'View or Edit recipe' page. These are differentiated by the 
-# mode argument passed from the appropriate page. 
-# Invoked from 'Recipes' page.
 @app.route('/view_or_edit_recipe/<_id>/<mode>')
 def view_or_edit_recipe(_id, mode):
+    """ Show either the 'View or Edit recipe' page. These are differentiated by the 
+    mode argument passed from the appropriate page. 
+    Invoked from 'Recipes' page. """
+
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(_id)})
 
     # The user_temperature_type variable determines the type the user originally selected
     # when adding the recipe to the database i.e. celsius, celsius fan, fahrenheit or gas mark. This 
     # is need when redisplaying the temperature for update because only one type is entered by the 
-    # user when adding the recipe. The rest are derived by formulae.
+    # user when adding the recipe. The rest are derived by formulae. The full set of temperatures 
+    # settings are only shown on the Home page or when viewing a recipe from the 'Recipes' page.
     user_temperature_type = recipe["temperature"]["user_temperature_type"]
 
     if user_temperature_type == 0:
@@ -106,17 +110,20 @@ def view_or_edit_recipe(_id, mode):
 
     recipe_categories = mongo.db.recipe_categories.find()
     recipe_category_list = [recipe_category for recipe_category in recipe_categories]
+    
     if mode == "view":
         template = "view_recipe.html"
-    if mode == "edit":
+    elif mode == "edit":
         template = "edit_recipe.html"
 
-    return render_template(template, recipe = recipe, user_temperature_type = user_temperature_type, temperature_value = temperature_value, recipe_categories = recipe_category_list, usernames=list(mongo.db.users.find()))
+    return render_template(template, recipe = recipe, user_temperature_type = user_temperature_type, 
+                           temperature_value = temperature_value, recipe_categories = recipe_category_list,
+                           usernames=list(mongo.db.users.find()))
 
-# Update the recipe and return to the 'Recipes' page.
-# Invoked from 'Edit recipe' page.
 @app.route('/update_recipe/<_id>', methods=["POST"])
 def update_recipe(_id):
+    """ Update the recipe and return to the 'Recipes' page.
+    # Invoked from 'Edit recipe' page."""
     recipes = mongo.db.recipes
 
     # Multi-line input elements need converted to a list before saving to MongoDB.
@@ -148,10 +155,11 @@ def update_recipe(_id):
     })
     return redirect(url_for('recipes'))
 
-# Set the recipe to 'archived', so that it's hidden, and return to the 'Recipes' page.
-# Invoked from 'Recipes' page.
 @app.route('/archive_recipe/<_id>')
 def archive_recipe(_id):
+    """ Set the recipe to 'archived', so that it's hidden, and return to the 'Recipes' page.
+    Invoked from 'Recipes' page. """
+
     recipes = mongo.db.recipes
     
     # Set the archived flag for this recipe.
@@ -160,60 +168,70 @@ def archive_recipe(_id):
         {"archived": True}
     })
     
-    # Refresh recipes page now that recipe has been archived and should no longer be displayed.
+    # Refresh recipes page now that recipe has been archived, as it should no longer be displayed.
     return redirect(url_for('recipes'))
 
-# Delete the recipe and return to the 'Recipes' page.
-# Invoked from 'Recipes' page.
 @app.route('/delete_recipe/<_id>')
 def delete_recipe(_id):
+    """ Delete the recipe and return to the 'Recipes' page.
+    Invoked from 'Recipes' page. """
 
     result = mongo.db.recipes.delete_one({"_id:": ObjectId(_id)})
     print("result = " + str(result))
 
-    # Refresh recipes page now that recipe has been deleted and should no longer be displayed.
+    # Refresh recipes page now that recipe has been deleted, as it should no longer be displayed.
     return redirect(url_for('recipes'))
 
-# Show the 'Recipe category' page. 
-# Invoked primarily from the Nav bar.
 @app.route('/recipe_categories')
 def recipe_categories():
-    return render_template("recipe_categories.html", recipe_categories=list(mongo.db.recipe_categories.find().sort("category_name", 1)))
+    """ Show the 'Recipe category' page. 
+    Invoked primarily from the Nav bar. """
+    
+    return render_template(
+        "recipe_categories.html", 
+        recipe_categories=list(mongo.db.recipe_categories.find().sort("category_name", 1)))
 
-# Show the 'Add recipe category' page. 
-# Invoked from 'Category' page.
 @app.route('/add_recipe_category')
 def add_recipe_category():
+    """ Show the 'Add recipe category' page. 
+    Invoked from 'Category' page. """
+    
     return render_template("add_recipe_category.html")
 
-# Insert the recipe category and return to the 'Recipe categories' page.
-# Invoked from 'Add recipe category' page.
 @app.route('/insert_recipe_category', methods=["POST"])
 def insert_recipe_category():
+    """ Insert the recipe category and return to the 'Recipe categories' page.
+    Invoked from 'Add recipe category' page. """
+    
     recipe_categories = mongo.db.recipe_categories
     recipe_category = {"category_name": request.form.get('category_name')}
     recipe_categories.insert_one(recipe_category)
     return redirect(url_for('recipe_categories'))
 
-# Show the 'Edit recipe category' page.
-# Invoked from 'Recipe category' page.
 @app.route('/edit_recipe_category/<recipe_category_id>')
 def edit_recipe_category(recipe_category_id):
-    return render_template("edit_recipe_category.html", recipe_category = mongo.db.recipe_categories.find_one({"_id": ObjectId(recipe_category_id)}))
+    """Show the 'Edit recipe category' page.
+    Invoked from 'Recipe category' page. """
 
-# Update the recipe category and return to the 'Recipe categories' page.
-# Invoked from 'Edit recipe category' page.
+    return render_template(
+        "edit_recipe_category.html", 
+        recipe_category = mongo.db.recipe_categories.find_one({"_id": ObjectId(recipe_category_id)}))
+
 @app.route('/update_recipe_category/<recipe_category_id>', methods=["POST"])
 def update_recipe_category(recipe_category_id):
+    """ Update the recipe category and return to the 'Recipe categories' page.
+    Invoked from 'Edit recipe category' page. """
+
     mongo.db.recipe_categories.update(
         {'_id': ObjectId(recipe_category_id)},
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('recipe_categories'))
 
-# Delete the recipe category and return to the 'Recipe categories' page.
-# Invoked from 'Recipe category' page.
 @app.route('/delete_recipe_category/<recipe_category_id>')
 def delete_recipe_category(recipe_category_id):
+    """ Delete the recipe category and return to the 'Recipe categories' page.
+    Invoked from 'Recipe category' page. """
+
     mongo.db.recipe_categories.delete_one({'_id': ObjectId(recipe_category_id)})
     return redirect(url_for('recipe_categories'))
 
